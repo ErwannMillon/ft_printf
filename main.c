140f953da7f27d7de5a045bb6d0b398575f3acb6
Glenn Millon <gmillon@c4r1p4.42nice.fr>
2022-04-02 17:03:06 +0200

    a

--- main.c
+++ main.c
@@ -10,22 +10,22 @@ int main (void)
 	// char *teststring = "ab%%cd%padf %p %pmasdf, %x";
 	// char *teststring = "ab%%cd%padf %p %pmasdf, %x";
 	// char *teststring = "ab%%cd%padf %p %pmasdf, %x";
-	// char *teststring = "ab%%cd%padf %p %pmasdf, %x";
-	char *teststring = "%i %i %i";
+	char *teststring = "ab%%cd  %p adf %i %pmasdf, %x";
+	// char *teststring = "%i %i %i";
 	
-	x = ft_printf(teststring,-20, -020, -0x2A);
+	x = ft_printf(teststring, &x, -020, -0x2A);
 	printf("     result: %d", x);
 	printf("\n");
-	x =    printf(teststring, -20, -020, -0x2A);
-	printf("\n%s\n\n", strerror(errno));
+	x =    printf(teststring, &x, -020, -0x2A);
+	// printf("\n%s\n\n", strerror(errno));
 
 	printf("     result: %d", x);
 
 	printf("\n");
 	// system("leaks a.out");
-	// ft_printf("abcd%dadf %d %umasdf, %x", -2147483648, -4567, 0, 435);
-	// printf("\n");
-	// printf("abcd%dadf %d %umasdf, %x", -2147483648, -4567, 0, 435);
+	ft_printf("abcd%iadf %d %umasdf, %x", -214748364999, -4567, 0, 435);
+	printf("\n");
+	printf("abcd%liadf %d %umasdf, %x", -214748364999, -4567, 0, 435);
 
-
+	// handle_p((long int)&x);
 }
